3d1ef126d3e84528e420a132afdb3f8519746ff2
mvisca <martin.visca@gmail.com>
2024-02-04 17:47:00 +0100

    Actualizar .gitignore

---
+++ .build/interface.d
@@ -0,0 +1,7 @@
+.build/interface.o: src/interface.c src/../include/minishell.h \
+ src/../include/macros.h src/../include/structures.h \
+ src/../include/../lib/libft/include/libft.h
+src/../include/minishell.h:
+src/../include/macros.h:
+src/../include/structures.h:
+src/../include/../lib/libft/include/libft.h:
