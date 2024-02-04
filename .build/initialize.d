8bff6500c919ddedda941a9565ba53bbe66abca5
mvisca <martin.visca@gmail.com>
2024-02-04 17:50:35 +0100

    clean

--- .build/initialize.d
+++
@@ -1,7 +0,0 @@
-.build/initialize.o: src/initialize.c src/../include/minishell.h \
- src/../include/macros.h src/../include/structures.h \
- src/../include/../lib/libft/include/libft.h
-src/../include/minishell.h:
-src/../include/macros.h:
-src/../include/structures.h:
-src/../include/../lib/libft/include/libft.h:
