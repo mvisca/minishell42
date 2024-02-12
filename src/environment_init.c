26086b134bffcc80cc2113b65060d4f4bcb74dfc
mvisca <martin.visca@gmail.com>
2024-02-12 19:17:58 +0100

    fix environment_init(....) to store correct VALUE for KEY when = is included

--- src/environment_init.c
+++ src/environment_init.c
@@ -1,5 +1,40 @@
 #include "../include/minishell.h"
 
+// Fix information lose when env var value includes the char '='
+static int	check_oversplit(t_envlst *new, char **pair)
+{
+	if (pair && ft_tablen(pair) > 2)
+	{
+		char	*aux;
+		int		i;
+
+		if (ft_tablen(pair) < 2)
+			return (1);
+		i = 2;
+		while (pair && pair[i])
+		{
+			ft_printf("\x1b[1;35mValue in new =\x1b[0m %s\n", new->value);
+			aux = new->value;
+			new->value = ft_strjoin(aux, "=");
+			free(aux);
+			aux = new->value;
+			new->value = ft_strjoin(aux, pair[i]);
+			free(aux);
+			i++;
+		}
+		
+		ft_printf("Key =\t'%s'\n", pair[0]);
+		ft_printf("Value =\t'%s'\n", pair[1]);
+		ft_printf("Join del value...%s\n", new->key);
+		ft_printf("Value =\n%s\n", new->value);
+		
+		// int i = 1;
+		// while (pair[i])
+		// 	ft_printf("Value=\t'%s'\n", pair[i++]);
+	}
+	return (0);
+}
+
 // Tested Ok
 t_envlst	*environment_init(t_minishell *ms)
 {
@@ -16,6 +51,7 @@ t_envlst	*environment_init(t_minishell *ms)
 		new = environment_new_node(ms, pair[0], pair[1]);
 		if (!new)
 			error_free_exit("malloc error", ms);
+		check_oversplit(new, pair);
 		environment_add_node(ms, new);
 		utils_free_tab(pair);
 		i++;
