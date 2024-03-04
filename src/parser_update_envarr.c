1f2d08f22078134be0630580ba38e6180407e1df
mvisca <martin.visca@gmail.com>
2024-03-04 22:52:54 +0100

    fix

--- src/parser_update_envarr.c
+++ src/parser_update_envarr.c
@@ -6,49 +6,11 @@
 /*   By: mvisca <mvisca@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 21:56:59 by mvisca            #+#    #+#             */
-/*   Updated: 2024/03/04 22:34:03 by mvisca           ###   ########.fr       */
+/*   Updated: 2024/03/04 22:52:49 by mvisca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
-
-// int	parser_init_redir(t_coml *command, t_tokl *token)
-// {
-// 	t_redl	*redir;
-
-// 	redir = command->redirect;
-// 	while (redir && redir->next)
-// 		redir = redir->next;
-// 	redir->path = ft_strdup(token->str);
-// 	redir->type = token->type;
-// 	redir->next = NULL;
-// 	return (0);
-// }
-
-// int	parser_alloc_add_redir(t_coml *command)
-// {
-// 	t_redl	*redir;
-// 	t_redl	*aux;
-
-// 	redir = (t_redl *)ft_calloc(1, sizeof(t_redl));
-// 	// redir = (t_redl *)supresedmalloc(sizeof(t_redl));
-// 	if (!redir)
-// 		return (1);
-// 	redir->type = 0;
-// 	redir->path = NULL;
-// 	redir->next = NULL;
-// 	aux = command->redirect;
-// 	if (!aux)
-// 	{
-// 		command->redirect = redir;
-// 		return (0);
-// 	}
-// 	while (aux->next)
-// 		aux = aux->next;
-// 	aux->next = redir;
-	
-// 	return (0);
-// }
 
 int parser_update_envarr(t_ms *ms)
 {
