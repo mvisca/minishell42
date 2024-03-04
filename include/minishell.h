6fa44ed60319f1ff4f6e0fc7aa22b5b3c4b4c55b
mvisca <martin.visca@gmail.com>
2024-03-04 16:26:51 +0100

    update

--- include/minishell.h
+++ include/minishell.h
@@ -69,11 +69,11 @@ t_tokl	    *lexer_add_token(t_ms *ms, t_tokl *token);
 int			parser(t_ms *ms);
 t_coml		*parser_new_command(t_coml **commnad);
 char        **parser_split(char *str);
-
-int			parser_add_redirect(t_coml *cmnd, t_redl *redir);
-int			parser_init_redir(t_coml *command, t_tokl *token);
-
 int			parser_add_command(t_ms *ms, t_coml *command);
+int			parser_add_redirect(t_coml *cmnd, t_redl *redir);
+
+// int			parser_init_redir(t_coml *command, t_tokl *token);
+
 
 /*##################	UTILS		#####################*/
 // Memory
