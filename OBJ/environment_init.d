091c7b51d68d9c8436d875c02b33cb63a0c38526
mvisca-g <mvisca-g@car15s6.42barcelona.com>
2024-03-20 14:16:58 +0100

    step

--- OBJ/environment_init.d
+++
@@ -1,6 +0,0 @@
-OBJ/environment_init.o: src/environment_init.c src/../include/minishell.h \
-  lib/readline/readline.h lib/readline/rlstdc.h \
-  lib/readline/rltypedefs.h lib/readline/keymaps.h \
-  lib/readline/chardefs.h lib/readline/tilde.h lib/readline/history.h \
-  src/../include/macros.h src/../include/structures.h \
-  src/../include/../lib/libft/include/libft.h
--- OBJ/parser_split.d
+++
@@ -1,6 +0,0 @@
-OBJ/parser_split.o: src/parser_split.c src/../include/minishell.h \
-  lib/readline/readline.h lib/readline/rlstdc.h \
-  lib/readline/rltypedefs.h lib/readline/keymaps.h \
-  lib/readline/chardefs.h lib/readline/tilde.h lib/readline/history.h \
-  src/../include/macros.h src/../include/structures.h \
-  src/../include/../lib/libft/include/libft.h
