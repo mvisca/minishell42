43782071c269dd92db46bf3c2284abdd11f55bee
mvisca-g <mvisca-g@car15s6.42barcelona.com>
2024-05-28 17:27:37 +0200

    add error management in builtin export

--- src/builtin_export.c
+++ src/builtin_export.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_export.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mvisca <mvisca@student.42.fr>              +#+  +:+       +#+        */
+/*   By: mvisca-g <mvisca-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 01:33:01 by mvisca            #+#    #+#             */
-/*   Updated: 2024/05/28 00:42:33 by mvisca           ###   ########.fr       */
+/*   Updated: 2024/05/28 17:26:40 by mvisca-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-static int	builtin_export_declare(t_envl *env,  t_ms *ms)
+static int	builtin_export_no_options(t_envl *env, t_ms *ms)
 {
 	while (env)
 	{
@@ -25,7 +25,19 @@ static int	builtin_export_declare(t_envl *env,  t_ms *ms)
 
 static int	builtin_export_check_option(t_coml *cmnd, t_ms *ms)
 {
+	int	fail;
 
+	fail = 0;
+	if ((!ft_strchr("qwertyuiopaasdfghjklzxcvbnm", cmnd->command[0])) \
+	|| (!ft_strchr("_QWERTYUIOPASDFGHJKLZXCVBNM", cmnd->command[0])))
+	{
+		ft_putstr_fd("export: '", 2);
+		ft_putstr_fd(cmnd, 2);
+		ft_putstr_fd("': not a valid identifier\n", 2);
+		ms->exit_code = 1;
+	}
+	// ir hasta el final recorriendo el string para ver donde esta el = y ver si el =+ va antes del =
+	// hacer el concat o reemplaza
 }
 
 int	builtin_export(t_coml *cmnd, t_ms *ms)
@@ -33,19 +45,20 @@ int	builtin_export(t_coml *cmnd, t_ms *ms)
 	int		i;
 	t_envl	*env;
 
+	i = 1;
 	env = ms->envlst;
 	if (cmnd->command[1] == NULL)
 	{
-		builtin_export_declare(env, ms);
+		builtin_export_no_options(env, ms);
 		return (0);
 	}
 	i = 1;
-	while (builtin_export_check_option(cmnd->command[i], ms))
+	while (cmnd->command[i], ms)
 	{
-		
+		builtin_export_check_option(cmnd->command[i], ms);
+		i++;
 	}
 
-	
 }
 
 
