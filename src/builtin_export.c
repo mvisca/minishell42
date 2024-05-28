c29b35c2b5a23dbab7dbe44db807362522f5c3f8
mvisca <martin.visca@gmail.com>
2024-05-29 01:49:17 +0200

    update

--- src/builtin_export.c
+++ src/builtin_export.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_export.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mvisca-g <mvisca-g@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mvisca <mvisca@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 01:33:01 by mvisca            #+#    #+#             */
-/*   Updated: 2024/05/28 17:26:40 by mvisca-g         ###   ########.fr       */
+/*   Updated: 2024/05/29 01:48:59 by mvisca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,38 @@ static int	builtin_export_no_options(t_envl *env, t_ms *ms)
 {
 	while (env)
 	{
-		ft_printf("declare -x %s=%s\n", env->key, env->value);
+		ft_printf("declare -x %s=\"%s\"\n", env->key, env->value);
 		env = env->next;
 	}
 	ms->exit_code = 0;
 	return (0);
 }
 
-static int	builtin_export_check_option(t_coml *cmnd, t_ms *ms)
+static int	builtin_export_check_options(t_coml *cmnd, t_ms *ms)
 {
 	int	fail;
+	int	i;
 
+	ms->exit_code = 0;
 	fail = 0;
-	if ((!ft_strchr("qwertyuiopaasdfghjklzxcvbnm", cmnd->command[0])) \
-	|| (!ft_strchr("_QWERTYUIOPASDFGHJKLZXCVBNM", cmnd->command[0])))
+	i = 0;
+	while ((ft_isalnum(cmnd->command[i])) || cmnd->command[i] == '_')
+		i++;
+	if ((!ft_strchr("qwertyuiopaasdfghjklñzxcvbnm", cmnd->command[0])) \
+	|| (!ft_strchr("_QWERTYUIOPASDFGHJKLÑZXCVBNM", cmnd->command[0])))
+		fail = 1;
+	if (fail || cmnd->command[i] != '=' && cmnd->command[i] != '\0')
 	{
 		ft_putstr_fd("export: '", 2);
 		ft_putstr_fd(cmnd, 2);
 		ft_putstr_fd("': not a valid identifier\n", 2);
 		ms->exit_code = 1;
 	}
+	else
+	{
+		// create, update, contact
+	}
+	return (ms->exit_code);
 	// ir hasta el final recorriendo el string para ver donde esta el = y ver si el =+ va antes del =
 	// hacer el concat o reemplaza
 }
@@ -55,7 +67,7 @@ int	builtin_export(t_coml *cmnd, t_ms *ms)
 	i = 1;
 	while (cmnd->command[i], ms)
 	{
-		builtin_export_check_option(cmnd->command[i], ms);
+		builtin_export_check_options(cmnd->command[i], ms);
 		i++;
 	}
 
