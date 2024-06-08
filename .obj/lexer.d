98a5c541f05967674b0b8752d16076adabc2fd50
Francisco Javier Català Balta <fcatala-@car6s6.42barcelona.com>
2024-06-08 15:48:26 +0200

    test porsiaca

---
+++ .obj/lexer.d
@@ -0,0 +1,5 @@
+.obj/lexer.o: src/lexer.c src/../include/minishell.h \
+ lib/readline/readline.h lib/readline/rlstdc.h lib/readline/rltypedefs.h \
+ lib/readline/keymaps.h lib/readline/chardefs.h lib/readline/tilde.h \
+ lib/readline/history.h src/../include/global.h src/../include/macros.h \
+ src/../include/structures.h src/../include/../lib/libft/include/libft.h
