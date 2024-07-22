559f3b8c9f783f8169b471a03984289a84121c19
Martin Visca Garat <mvisca-g@car9s5.42barcelona.com>
2024-07-22 04:54:20 +0200

    echo fix

--- src/builtin_echo.c
+++ src/builtin_echo.c
@@ -6,7 +6,7 @@
 /*   By: mvisca <mvisca@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 22:36:21 by mvisca            #+#    #+#             */
-/*   Updated: 2024/07/22 04:42:21 by mvisca-g         ###   ########.fr       */
+/*   Updated: 2024/07/22 04:50:17 by mvisca-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ static int	echo_print(char **com, int i)
 	str = ft_strtrim(com[i], SPACES);
 	while (str[j])
 	{
-		if (str[j] != '\n')
+		if (str[j] != '\n' && str[j] > 0 && str[j] != '\\')
 			count = printf("%c", str[j]);
 		j++;
 	}
