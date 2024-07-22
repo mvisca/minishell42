4a4ce5a0062cf0a7e51ce9f28142aab02a4a069a
Martin Visca Garat <mvisca-g@car9s5.42barcelona.com>
2024-07-22 07:52:44 +0200

    done

--- .obj/expander_var_types.d
+++
@@ -1,6 +0,0 @@
-.obj/expander_var_types.o: src/expander_var_types.c \
- src/../include/minishell.h lib/readline/readline.h lib/readline/rlstdc.h \
- lib/readline/rltypedefs.h lib/readline/keymaps.h lib/readline/chardefs.h \
- lib/readline/tilde.h lib/readline/history.h src/../include/global.h \
- src/../include/macros.h src/../include/structures.h \
- src/../include/../lib/libft/include/libft.h
